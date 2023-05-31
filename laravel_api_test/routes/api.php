360aa04d60e284408530ad64eaab2ecece9be76a
tkenmoku <tatsuya.kenmoku@milplateaux.com>
2023-05-31 19:46:20 +0900

    Update api.php

--- laravel_api_test/routes/api.php
+++ laravel_api_test/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\PostsController;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
